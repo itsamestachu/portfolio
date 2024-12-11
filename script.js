adf2aa5f51bd9fe9828f0018da0a7bf202765dc4
Stachu <101731@glr.nl>
2024-12-11 19:08:24 +0100

    stars in the background

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ projectItems.forEach(item => {
             popup.style.display = "block";
         }
 
+
         const video = popup.querySelector("video");
         if (video) {
             video.play();
@@ -18,6 +19,7 @@ projectItems.forEach(item => {
         if (popup) {
             popup.style.display = "none";
         }
+        
 
         const video = popup.querySelector("video");
         if (video) {
@@ -25,3 +27,61 @@ projectItems.forEach(item => {
         }
     });
 });
+
+const canvas = document.getElementById('stars');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const stars = [];
+const maxStars = 1000;
+
+for (let i = 0; i < maxStars; i++) {
+    stars.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        radius: Math.random(),
+        vx: Math.random() * 0.5,
+        vy: Math.random() * 0.5,
+        opacity: Math.random(),
+    });
+}
+
+function getStar() {
+    return {
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        radius: Math.random(),
+        vx: Math.random() * 0.5,
+        vy: Math.random() * 0.5,
+        opacity: Math.random(),
+    };
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < maxStars; i++) {
+        const star = stars[i];
+        ctx.beginPath();
+        ctx.arc(star.x, star.y, star.radius, 0, 2 * Math.PI);
+        ctx.fillStyle = 'white';
+        ctx.globalAlpha = star.opacity;
+        ctx.fill();
+        star.x += star.vx;
+        star.y += star.vy;
+
+        if (star.x < 0 || star.x > canvas.width) {
+            star.vx = -star.vx;
+        }
+
+        if (star.y < 0 || star.y > canvas.height) {
+            star.vy = -star.vy;
+        }
+    }
+
+    requestAnimationFrame(draw);
+}
+
+draw();
